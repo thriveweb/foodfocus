16ca4cb8c870568d933ef3dc085b90bce2211f94
Amber Gowans <amber@thriveweb.com.au>
2018-04-09 09:00:50 +1000

    message

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,30 +1,35 @@
 import React from 'react'
 
 import './Footer.css'
+import LazyImage from './LazyImage'
 
 export default ({content, phone, email, socialMedias }) => (
-  <footer className='Footer'>
+  <footer className='footer'>
   	<div className='container'>
   		<div className='footer--col-left'>
-  			<img src='/images/uploads/logo.300.png' alt='Food Focus Logo' />
+  			<LazyImage src='/images/uploads/logo.png' imageSize='300' alt='Food Focus Logo' />
   			<p>{content}</p>
   		</div>
   		<div className='footer--col-middle'>
   		    <p>Phone: <a href={`tel:${phone}`}>{phone}</a></p>
   			<p>Email: <a href={`mailto:${email}`}>{email}</a></p>
   			<div className='footer--social-media'>
-
+  				{socialMedias.map(socialMedia => {
+  					return <a href={socialMedia.accountUrl}>{socialMedia.icon}</a>
+  				})}
   			</div>
   		</div>
+  		<div className='footer--col-right'>
+  		</div>
   	</div>
-    <div className='Footer--Lower'>
+    <div className='footer--bottom'>
       <div className='container'>
         <p>© 2018 Copyright Food Focus</p>
         <a href="https://thriveweb.com.au/" title="Web Design Gold Coast" >Web Design Gold Coast</a> - THRIVE
         <nav>
         	<ul>
-        		<li><a href='/'></a></li>
-        		<li><a href='/'></a></li>
+        		<li><a href='/'>FAQ</a></li>
+        		<li><a href='/'>Terms & Conditions</a></li>
         	</ul>	
         </nav>
       </div>
@@ -32,8 +37,4 @@ export default ({content, phone, email, socialMedias }) => (
   </footer>
 )
 
-
-  				// {socialMedias.map(socialMedia => {
-  				// 	return <a href='/'>{socialMedia.icon}</a>
-  				// })}
 
