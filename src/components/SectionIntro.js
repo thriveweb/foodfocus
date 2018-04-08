7c98b29b0719b9d0332bcb2e8169b96ea0d77e34
Amber Gowans <amber@thriveweb.com.au>
2018-04-08 11:26:27 +1000

    commit

--- src/components/SectionIntro.js
+++ src/components/SectionIntro.js
@@ -1,10 +1,10 @@
 import React from 'react'
 
-export defualt ({title, content, buttonUrl, buttonText}) => (
+export default ({heading, content, buttonUrl, buttonText}) => (
 	<section className='section--intro'>
 		<div className='container'>
 			<div className='section--intro-content'>
-				<h2>{title}</h2>
+				<h2>{heading}</h2>
 				<p>{content}</p>
 			</div>
 			<a className='button' href={`${buttonUrl}`}>{buttonText}</a>
--- src/views/Home.js
+++ src/views/Home.js
@@ -4,14 +4,17 @@ import Helmet from 'react-helmet'
 // import Content from '../components/Content'
 import Banner from '../components/Banner'
 import ImageGallery from '../components/ImageGallery'
+import SectionIntro from '../components/SectionIntro'
 import './Home.css'
 
 export default ({ page, globalSettings }) => {
   const { featuredImage, title, address, phone } = page
+  const { heading, content, ButtonText, ButtonUrl} = page
 
   return (
     <main className='Home'>
       <Banner image={featuredImage} heading={title} address={address} phone={phone} />
+      <SectionIntro heading={heading} content={content} ButtonText={ButtonText} ButtonUrl={ButtonUrl} />
       <ImageGallery galleryItems={page.galleryImages} />
       <Helmet>
         <title>{title}</title>
