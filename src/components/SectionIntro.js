6d20e6c17ffaee30547d6a47c020671784e0578d
Amber Gowans <amber@thriveweb.com.au>
2018-04-08 11:15:40 +1000

    latest-commit

---
+++ src/components/SectionIntro.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export defualt ({title, content, buttonUrl, buttonText}) => (
+	<section className='section--intro'>
+		<div className='container'>
+			<div className='section--intro-content'>
+				<h2>{title}</h2>
+				<p>{content}</p>
+			</div>
+			<a className='button' href={`${buttonUrl}`}>{buttonText}</a>
+		</div>
+	</section>
+)
