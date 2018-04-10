dac8b9cc2bccfcad8bddd2baf308d4a388b9524e
Amber Gowans <amber@thriveweb.com.au>
2018-04-10 12:44:33 +1000

    message

--- src/components/DefaultTemplate.js
+++ src/components/DefaultTemplate.js
@@ -5,12 +5,12 @@ import './DefaultTemplate.css'
 export default({title, contentSection}) => (
 	<section className='section--default-template'>
 		<div className='container'>
-			<h1>{title}</h1>
+			{title ? <h1>{title}</h1> : ''}
 			<div className='section--default-template-content'>
 				{contentSection.map(content => {
 					return<div key={`${content.heading}`} className='section--default-template-content-item'>
-						<h3>{content.heading}</h3>
-						<p>{content.content}</p>
+						{content.heading ? <h3>{content.heading}</h3> : ''}
+						{content.content ? <p>{content.content}</p> : ''}
 					</div>
 				})}
 			</div>
